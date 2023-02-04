31687bb4e38e1fb0463735f595bce40d080def73
samirvithlani <samir.vithlani83955@gmail.com>
2023-02-04 14:44:37 +0530

    first session

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Header } from "./Header";
+import Footer from "./Footer";
 
 function App() {
+
+  var style ={
+    color: "red",
+  }
+  var no1 = 10;
+  var per = 12.5;
+  var name = "John";
+  var isTrue = false;
+  var user = {
+    name: "John",
+    age: 30,
+    isMarried: true,
+    city: "New York",
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header/>
+      <div>
+        <h1>No1 = {no1}</h1>
+        <h1>Per = {per}</h1>
+        <h1>Name = {name}</h1>
+        <h1>isTrue = {isTrue == true ? "TRUE" : "FALSE"}</h1>
+        <h2 style={style}>{user.name}</h2>
+        <h2>{user.age}</h2>
+        <h2>{user.isMarried ? "yes" : "no"}</h2>
+        <h2>{user.city}</h2>
+        <Footer/>
+      </div>
+    </>
   );
 }
 
---
+++ src/Header.js
@@ -0,0 +1,12 @@
+//class based component functional based component
+//arrow function , function keyword
+
+import React from 'react'
+import "./App.css";
+
+//component name must be capital
+export const Header = () => {
+  return (
+    <div className='header'>Header</div>
+  )
+}
